84f01de98ed607af77e165873690191b45eba4ad
DingiDinigi <uzoamakaagwu5@gmail.com>
2026-07-06 15:52:53 +0200

    chore: track frontend build output and unignore dist/ Nginx serves frontend/dist directly; dist must be in git for deployments to pick up rebuilt assets.

---
+++ frontend/dist/assets/arrow-left-DwGQy57i.js
@@ -0,0 +1,8 @@
+import{w as o}from"./core-CgHL6AJe.js";import"./index-C8XX51Kl.js";import"./index-BbP3371Q.js";import"./index.es-C0ZNsp5I.js";import"./index-nibyPLVP.js";const p=o`<svg fill="none" viewBox="0 0 14 15">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M13 7.99a1 1 0 0 1-1 1H4.4l2.46 2.46a1 1 0 1 1-1.41 1.41L1.29 8.7a1 1 0 0 1 0-1.41L5.46 3.1a1 1 0 0 1 1.41 1.42L4.41 6.99H12a1 1 0 0 1 1 1Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{p as arrowLeftSvg};
